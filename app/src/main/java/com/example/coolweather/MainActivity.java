6a46a9c63c5deeeff447adbf231e7b2fa2360be4
dell <xingruijun@sina.cn>
2018-06-03 13:49:50 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.coolweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
